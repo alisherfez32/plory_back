bda7ca9fdbfa55d771687d549351772034ce0da2
Dadakhon <dadakhonabdulloev@gmail.com>
2022-05-21 23:31:31 +0300

    changed mediafiles

--- backend/settings.py
+++ backend/settings.py
@@ -158,7 +158,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Default primary key field type
